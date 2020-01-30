e461b018ee47e090504c57e9741ba4504d24bd9e
Jaeyoon Park <jaeyoon1015@naver.com>
2020-01-30 15:24:13 +0900

    0130_3

--- controllers/home.js
+++ controllers/home.js
@@ -8,29 +8,17 @@ const Product = require('../models/shoppingList')
 
 exports.index = (req, res) => {
 
-  //   shoppingList.insert({ id:'1', image:'1.jpg', price: 1000, explanation1: 'hi',explanation2: 'ho', point: 100}).then(function(results) {
-  //     // console.log('== Resolved\n', results);
-  //     console.log('Promise Based Insert Result : ', results);
-  //  }, function(err) {
-  //     console.log('== Rejected\n', err);      
-  //  });
-
-  //   shoppingList.find().toArray(function (err, docs) {
-  //     if(err) console.log(err + " home.js ");
-  //     console.log('docs: ' + docs);
-  //  });
-
   Product.find({})
   .then((products) => {
-    console.log(products)
-    console.log(products.toString)
+    // console.log(products)
+    // console.log(products.toString)
     const datas = products;
     const jsonObject = JSON.stringify(datas)
     const test = JSON.parse(jsonObject);
     //console.log(" price: " + test[0].price)
     res.render('home', {
       title: 'Home',
-      products : jsonObject
+      products : test
     });
     
   })
@@ -43,32 +31,6 @@ exports.index = (req, res) => {
 
 exports.dbtest = (req, res) => {
 
-
-  // User.find({})
-  //   .then((users) => {
-  //     console.log(users)
-  //   })
-  //   .catch((err) => {
-  //     console.log(err)
-  //   })
-
-  Product.find({})
-  .then((products) => {
-    console.log(products)
-    console.log(products.toString)
-    const datas = products;
-    const jsonObject = JSON.stringify(datas)
-    const test = JSON.parse(jsonObject);
-    console.log(" price: " + test[0].price)
-    
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-
-  res.render('home', {
-    title: 'Home'
-  });
 };
 
 
